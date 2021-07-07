8f9ea5d05753dc04b556a4438c82d3e5288df273
Gagan <g.deepsingh1@gmail.com>
2021-07-07 19:52:23 +0530

    provisioner destroy

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -2,4 +2,8 @@ resource "null_resource" "dummy_resource" {
     provisioner "local-exec" {
         command = "echo '0' > status.txt"
     }
+    provisioner "local-exec" {
+        when = destroy
+        command = "echo '1' > status.txt"
+    }
 }
